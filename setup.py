5f040f87293e726c7391aacf3997b8029832b6f9
Joachim Bingel <joa.bingel@gmail.com>
2019-03-28 22:46:38 +0100

    update setup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup
 
-VERSION = '0.2'
+VERSION = '0.2.1'
 
 setup(
     name='rippletagger',
@@ -11,15 +11,15 @@ setup(
     description='RippleTagger identifies part-of-speech tags (NOUN, VERB...).',
     author=u'Emil Stenström',
     author_email='em@kth.se',
-    url='https://github.com/EmilStenstrom/rippletagger/',
-    download_url='https://github.com/EmilStenstrom/rippletagger/archive/%s.zip' % VERSION,
+    url='https://github.com/jbingel/rippletagger/',
+    download_url='https://github.com/jbingel/rippletagger/archive/%s.zip' % VERSION,
     install_requires=[],
     tests_require=["nose>=1.3.7", "flake8>=3.0.4"],
     test_suite="nose.collector",
     keywords=['pos-tagging', 'pos-tagger', 'multi-langauge', 'nlp', 'rippletagger'],
     classifiers=[
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.7",
         "Operating System :: OS Independent",
     ],
 )
